ac6b7a65c8ef38b1d9f1e1560ac7a78fa2f7e969
vladrepo <vladyslavkryzhnyi@gmail.com>
2020-04-21 10:08:04 +0300

    update day-view to hooks

--- src/components/pages/day-view.js
+++ src/components/pages/day-view.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import moment from "moment";
 import WeekLine from "../subcomponents/calendar/week-line";
 import MainHeader from "../subcomponents/header/main-header";
@@ -22,66 +22,64 @@ import {
 } from "../../store/actions";
 import BackCalendar from "../subcomponents/button/back-calendar";
 
-class DayView extends React.Component {
-  componentDidMount() {
+function DayView(props) {
+  useEffect(() => {
     const {
       getAvailableTimeSlots,
       getAvailableAppointments,
       setFlowMode1,
       setFlowMode2,
-    } = this.props;
+    } = props;
     getAvailableTimeSlots();
     getAvailableAppointments();
-    this.checkFlowisOne() ? setFlowMode1() : setFlowMode2();
-  }
+    checkFlowisOne() ? setFlowMode1() : setFlowMode2();
+  });
 
-  checkFlowisOne() {
-    const { mode } = this.props;
+  function checkFlowisOne() {
+    const { mode } = props;
     return mode === modes.flow1;
   }
 
-  render() {
-    const { onBackMonth1, onBackMonth2, timeSlots, selectedDate } = this.props;
+  const { onBackMonth1, onBackMonth2, timeSlots, selectedDate } = props;
 
-    const month = moment(selectedDate, DATE_FORMAT).month();
-    const headerByFlow = this.checkFlowisOne()
-      ? HEADERS.calendar
-      : HEADERS.testDriveDate;
+  const month = moment(selectedDate, DATE_FORMAT).month();
+  const headerByFlow = checkFlowisOne()
+    ? HEADERS.calendar
+    : HEADERS.testDriveDate;
 
-    const HeaderView = <MainHeader title={headerByFlow} />;
+  const HeaderView = <MainHeader title={headerByFlow} />;
 
-    const onBackMonth = () =>
-      this.checkFlowisOne() ? onBackMonth1() : onBackMonth2();
+  const onBackMonth = () =>
+    checkFlowisOne() ? onBackMonth1() : onBackMonth2();
 
-    const WeeksView = (
-      <div className="weeks-view">
-        <WeekLine />
-        <WeekView />
-      </div>
-    );
+  const WeeksView = (
+    <div className="weeks-view">
+      <WeekLine />
+      <WeekView />
+    </div>
+  );
 
-    const MainView = (
-      <>
-        <BackCalendar onClick={onBackMonth} label={months[month]} />
-        {WeeksView}
-        <SelectedDate />
-      </>
-    );
+  const MainView = (
+    <>
+      <BackCalendar onClick={onBackMonth} label={months[month]} />
+      {WeeksView}
+      <SelectedDate />
+    </>
+  );
 
-    const SlotsView = this.checkFlowisOne() ? (
-      <Appointments />
-    ) : (
-      <TimeSlots timeSlots={timeSlots} />
-    );
+  const SlotsView = checkFlowisOne() ? (
+    <Appointments />
+  ) : (
+    <TimeSlots timeSlots={timeSlots} />
+  );
 
-    return (
-      <div className="p-4 text-center vh-100">
-        {HeaderView}
-        {MainView}
-        {SlotsView}
-      </div>
-    );
-  }
+  return (
+    <div className="p-4 text-center vh-100">
+      {HeaderView}
+      {MainView}
+      {SlotsView}
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
